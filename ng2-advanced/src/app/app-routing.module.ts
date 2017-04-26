55e861f1c2027e531a9294e558e46522a84e454b
Mason Chu <masonchu@gmail.com>
2017-04-26 09:53:03 +0800

    [feature] 02 練習使用 routerLink 建立路由連結

--- ng2-advanced/src/app/app-routing.module.ts
+++ ng2-advanced/src/app/app-routing.module.ts
@@ -16,7 +16,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    enableTracing: true
+  })],
   exports: [RouterModule],
   providers: []
 })
