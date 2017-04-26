2ddabcf38e5129e9c6a26ad504ee6f734c1b2545
Mason Chu <masonchu@gmail.com>
2017-04-26 11:36:11 +0800

    [feature] 05 練習套用 WB0901P31-HTML5.zip 網頁版型並設定路由顯示 /dashboard 與 /cards 頁面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
@@ -5,9 +7,9 @@ import { Page2Component } from './page2/page2.component';
 import { fallbackRoute } from 'app/shared/fullback-route';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'page1', pathMatch: 'full' },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'card', component: CardComponent },
   fallbackRoute
 ];
 
