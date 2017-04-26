a48ff223c274f21a473a8a46bbdb90d5810069b0
Mason Chu <masonchu@gmail.com>
2017-04-26 10:28:21 +0800

    [feature] 04 練習預設路由、轉向路由、萬用路由、獨立路由物件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { fallbackRoute } from 'app/shared/fullback-route';
 
 const routes: Routes = [
-  {
-    path: 'page1',
-    component: Page1Component
-    // children: []
-  },
-  {
-    path: 'page2',
-    component: Page2Component
-  }
+  { path: '', redirectTo: 'page1', pathMatch: 'full' },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  fallbackRoute
 ];
 
 @NgModule({
